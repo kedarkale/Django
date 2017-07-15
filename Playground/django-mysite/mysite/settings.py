99529e6a7b78e6b2387ce27ad0ac471360dd63b9
Kedar K. Kale <kedar30092010@gmail.com>
2017-07-15 16:18:33 +0530

    Update settings.py

--- Playground/django-mysite/mysite/settings.py
+++ Playground/django-mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(o6i07e=)v%*vojti3v+j+%&n=jj3lbh_54k+m^2017kti4&#('
+SECRET_KEY = '(o6i07e=)28v5oitj3v+i+%&n=ii3lbh_54k+m^2017g9i4&#('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
