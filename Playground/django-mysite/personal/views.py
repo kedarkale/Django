5c2d38cff9c3cff414a9a2448151fdf3738f48ed
Kedar K. Kale <kedar30092010@gmail.com>
2017-07-17 19:20:45 +0530

    improved UI

--- Playground/django-mysite/personal/views.py
+++ Playground/django-mysite/personal/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request):
 	return render(request,'personal/header.html')
+
+def contact (request):
+	return render(request,'personal/contact/header.html',{'content':['contact me at : ','abc@abc.com']})
